341079cb543db9be976a8abe3b51baf4d255fca5
gkunze <gkunze@xceptance.net>
2024-03-28 14:34:59 +0100

    Added Threshhold

--- cypress/e2e/visual_homepagetest.cy.js
+++ cypress/e2e/visual_homepagetest.cy.js
@@ -3,7 +3,13 @@ import HomePage from "./pages/homepage"
 describe('Homepage visual test', () => {
   it('open homepage and compare with screenshot', () => {
     HomePage.open()
-    cy.compareSnapshot('startingpage');
-    cy.get('#carousel-product-display').compareSnapshot('product_display')
+    cy.compareSnapshot({
+      name: 'startingpage',
+      testThreshold: 0.2
+    });
+    cy.get('#carousel-product-display').compareSnapshot({
+        name: 'product_display',
+        testThreshold: 0.2
+      })
   })
 })
