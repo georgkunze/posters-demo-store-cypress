1fb8f62d97fc59ac0d3c94cc08e07ef34c32869c
gkunze <gkunze@xceptance.net>
2024-03-28 14:43:44 +0100

    Set fixed resolution for screenshot test

--- cypress/e2e/visual_homepagetest.cy.js
+++ cypress/e2e/visual_homepagetest.cy.js
@@ -3,9 +3,14 @@ import HomePage from "./pages/homepage"
 describe('Homepage visual test', () => {
   it('open homepage and compare with screenshot', () => {
     HomePage.open()
+    cy.viewport(1000, 660)
     cy.compareSnapshot({
       name: 'startingpage',
-      testThreshold: 0.2
+      testThreshold: 0.2,
+      retryOptions: {
+        limit: 5,
+        delay: 500
+      }
     });
     cy.get('#carousel-product-display').compareSnapshot({
         name: 'product_display',
